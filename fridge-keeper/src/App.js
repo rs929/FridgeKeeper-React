b09c1a5745836fc8c93bf78c03620124d69ecdd5
Richie Sun <rs929@cornell.edu>
2023-08-05 23:34:56 -0400

    Setup Header and updated Description

--- fridge-keeper/src/App.js
+++ fridge-keeper/src/App.js
@@ -4,19 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Fridge Keeper</h1>
+        <h3>Fridge Keeper is a user-friendly and innovative website that revolutionizes the way you manage your kitchen and cooking routines. This smart app helps you keep track of the items in your fridge and pantry and matches them with personalized recipes, making meal prep simple and sustainable.</h3>
       </header>
     </div>
   );
